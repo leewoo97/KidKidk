93e07bbefb66c9dc75197a70bdd3de34af613e0b
정준수 <junsjung137@gmail.com>
2024-01-29 20:29:51 +0900

    FundReservationDto 추가

--- backend/src/main/java/com/ssafy/kdkd/domain/dto/fund/FundReservationDto.java
+++ backend/src/main/java/com/ssafy/kdkd/domain/dto/fund/FundReservationDto.java
@@ -1,2 +1,28 @@
-package com.ssafy.kdkd.domain.dto.fund;public class FundReservationDto {
+package com.ssafy.kdkd.domain.dto.fund;
+
+import lombok.Data;
+
+@Data
+public class FundReservationDto {
+
+    private Long id;
+
+    private String name;
+
+    private String content;
+
+    private int yield;
+
+    private boolean state;
+
+    private Long childId;
+
+    public FundReservationDto(Long id, String name, String content, int yield, boolean state, Long childId) {
+        this.id = id;
+        this.name = name;
+        this.content = content;
+        this.yield = yield;
+        this.state = state;
+        this.childId = childId;
+    }
 }

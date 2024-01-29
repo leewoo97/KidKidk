343f6ea98ea2457228d7410be23a790deb7a2a22
정준수 <junsjung137@gmail.com>
2024-01-29 21:02:37 +0900

    Add: Saving respository, service 구현

--- backend/src/main/java/com/ssafy/kdkd/repository/saving/SavingRepository.java
+++ backend/src/main/java/com/ssafy/kdkd/repository/saving/SavingRepository.java
@@ -1,2 +1,8 @@
-package com.ssafy.kdkd.repository.saving;public interface SavingRepository {
+package com.ssafy.kdkd.repository.saving;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.kdkd.domain.entity.saving.Saving;
+
+public interface SavingRepository extends JpaRepository<Saving, Long> {
 }
--- backend/src/main/java/com/ssafy/kdkd/service/saving/SavingService.java
+++ backend/src/main/java/com/ssafy/kdkd/service/saving/SavingService.java
@@ -1,2 +1,31 @@
-package com.ssafy.kdkd.service.saving;public class SavingService {
+package com.ssafy.kdkd.service.saving;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.kdkd.domain.entity.saving.Saving;
+import com.ssafy.kdkd.repository.saving.SavingRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SavingService {
+
+    private final SavingRepository savingRepository;
+
+    public void save(Saving saving) {
+        savingRepository.save(saving);
+    }
+
+    public Optional<Saving> findById(Long childId) {
+        return savingRepository.findById(childId);
+    }
+
+    public void delete(Saving saving) {
+        savingRepository.delete(saving);
+    }
 }

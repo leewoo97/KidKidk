17710a32951cd4170f0302e30802c531f5eccdad
정준수 <junsjung137@gmail.com>
2024-01-29 15:39:00 +0900

    Add: SavingHistoryService 구현

--- backend/src/main/java/com/ssafy/kdkd/service/saving/SavingHistoryService.java
+++ backend/src/main/java/com/ssafy/kdkd/service/saving/SavingHistoryService.java
@@ -1,2 +1,28 @@
-package com.ssafy.kdkd.service.saving;public class SavingHistoryService {
+package com.ssafy.kdkd.service.saving;
+
+import com.ssafy.kdkd.domain.entity.saving.SavingHistory;
+import com.ssafy.kdkd.repository.saving.SavingHistoryRepository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SavingHistoryService {
+
+    private final SavingHistoryRepository savingHistoryRepository;
+
+    public void save(SavingHistory savingHistory) {
+        savingHistoryRepository.save(savingHistory);
+    }
+
+    public List<SavingHistory> findSavingHistoriesByChild_Id(Long childId) {
+        return savingHistoryRepository.findSavingHistoriesByChild_Id(childId);
+    }
+
 }

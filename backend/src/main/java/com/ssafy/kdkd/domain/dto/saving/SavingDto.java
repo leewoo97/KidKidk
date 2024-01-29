dd9ebdf7a31da59f4ba66bae592e67c8ed4b0bcd
정준수 <junsjung137@gmail.com>
2024-01-29 21:01:55 +0900

    Add: Saving entity에 연관관계 메서드, 생성자 추가(SavingDto 추가)

--- backend/src/main/java/com/ssafy/kdkd/domain/dto/saving/SavingDto.java
+++ backend/src/main/java/com/ssafy/kdkd/domain/dto/saving/SavingDto.java
@@ -1,4 +1,30 @@
 package com.ssafy.kdkd.domain.dto.saving;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class SavingDto {
+
+    private Long id;
+
+    private LocalDateTime startDate;
+
+    private int count;
+
+    private int payment;
+
+    private int rate;
+
+    private Long childId;
+
+    public SavingDto(Long id, LocalDateTime startDate, int count, int payment, int rate, Long childId) {
+        this.id = id;
+        this.startDate = startDate;
+        this.count = count;
+        this.payment = payment;
+        this.rate = rate;
+        this.childId = childId;
+    }
 }
---
+++ backend/src/main/java/com/ssafy/kdkd/service/saving/SavingService.java
@@ -0,0 +1,2 @@
+package com.ssafy.kdkd.service.saving;public class SavingService {
+}

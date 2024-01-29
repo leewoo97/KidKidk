2043e229f50fa87f334e7bb4f2fadd406dd9117d
정준수 <junsjung137@gmail.com>
2024-01-29 16:12:25 +0900

    Add: FundHistory repository 구현

--- backend/src/main/java/com/ssafy/kdkd/repository/fund/FundHistoryRepository.java
+++ backend/src/main/java/com/ssafy/kdkd/repository/fund/FundHistoryRepository.java
@@ -1,2 +1,16 @@
-package com.ssafy.kdkd.repository.fund;public interface FundHistoryRepository {
+package com.ssafy.kdkd.repository.fund;
+
+import com.ssafy.kdkd.domain.entity.fund.FundHistory;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface FundHistoryRepository extends JpaRepository<FundHistory, Long> {
+
+    @Query("SELECT fh FROM FundHistory fh WHERE fh.child.id = :childId")
+    List<FundHistory> findFundHistoryByChildId(@Param("childId") Long childId);
+
 }

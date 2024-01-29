64b99e4bbe6c0a095654742d5a2e630f98062338
정준수 <junsjung137@gmail.com>
2024-01-29 15:37:23 +0900

    Add: SavingHistory repository 구현

--- backend/src/main/java/com/ssafy/kdkd/repository/saving/SavingHistoryRepository.java
+++ backend/src/main/java/com/ssafy/kdkd/repository/saving/SavingHistoryRepository.java
@@ -1,2 +1,16 @@
-package com.ssafy.kdkd.repository.saving;public interface SavingHistoryRepository {
+package com.ssafy.kdkd.repository.saving;
+
+import com.ssafy.kdkd.domain.entity.saving.SavingHistory;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SavingHistoryRepository extends JpaRepository<SavingHistory, Long> {
+
+    @Query("SELECT sh FROM SavingHistory sh WHERE sh.child.id = :childId")
+    List<SavingHistory> findSavingHistoriesByChild_Id(@Param("childId") Long childId);
+
 }

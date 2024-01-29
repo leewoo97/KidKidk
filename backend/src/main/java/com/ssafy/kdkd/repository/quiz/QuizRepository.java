a19e871acdb73562e3fcb17806d5d41e0cccc40c
정준수 <junsjung137@gmail.com>
2024-01-29 14:47:38 +0900

    Add: Quiz repository 구현

--- backend/src/main/java/com/ssafy/kdkd/repository/quiz/QuizRepository.java
+++ backend/src/main/java/com/ssafy/kdkd/repository/quiz/QuizRepository.java
@@ -1,11 +1,11 @@
 package com.ssafy.kdkd.repository.quiz;
 
-import com.ssafy.kdkd.domain.entity.quiz.Quiz;
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import com.ssafy.kdkd.domain.entity.quiz.Quiz;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
 

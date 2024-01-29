d94a042fe1f542b40418b4e2cc32a5b116df62fa
정준수 <junsjung137@gmail.com>
2024-01-29 20:28:41 +0900

    Add: FundReservation repository, service 구현

--- backend/src/main/java/com/ssafy/kdkd/repository/fund/FundReservationRepository.java
+++ backend/src/main/java/com/ssafy/kdkd/repository/fund/FundReservationRepository.java
@@ -1,2 +1,8 @@
-package com.ssafy.kdkd.repository.fund;public interface FundReservationRepository {
+package com.ssafy.kdkd.repository.fund;
+
+import com.ssafy.kdkd.domain.entity.fund.FundReservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FundReservationRepository extends JpaRepository<FundReservation, Long> {
 }

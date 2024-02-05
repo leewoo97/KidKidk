eb9c2efb14414b6f47c1785a7aec607f60b7e7a8
chanmyeongwoo <chanmyeongwoo@naver.com>
2024-02-05 20:10:01 +0900

    Add: api folders

--- frontend/src/views/parent/ParentJob.jsx
+++ frontend/src/views/parent/ParentJob.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 
+import { getRetrieve } from '@api/fund.js';
+
 import styles from './ParentJob.module.css';
 import kidImg from '@images/kidImg.jpg';
 

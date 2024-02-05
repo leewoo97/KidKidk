88d1859806c6b769da31ccfddf1d589cac2351b8
한성현[대전3반_B305]팀원 <tjdgus4560@naver.com>
2024-02-05 09:54:32 +0900

    Chore: 백엔드 베이스 주소 변수명 변경

--- frontend/src/configs/config.js
+++ frontend/src/configs/config.js
@@ -1,5 +1,5 @@
 // 백엔드 주소
-export const backendURL = import.meta.env.VITE_SERVICE_BASE_URL;
+export const backendURL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 // 카카오 rest api 키
 export const kakaoRestApiKey = import.meta.env.VITE_REACT_APP_KAKAO_REST_API_KEY;

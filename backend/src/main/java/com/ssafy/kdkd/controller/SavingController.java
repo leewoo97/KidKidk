097d77c158f25cdb067e28c9e621d882525649f4
정준수 <junsjung137@gmail.com>
2024-02-05 20:35:18 +0900

    Fix: cros

--- backend/src/main/java/com/ssafy/kdkd/controller/SavingController.java
+++ backend/src/main/java/com/ssafy/kdkd/controller/SavingController.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@CrossOrigin("*")
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/saving")

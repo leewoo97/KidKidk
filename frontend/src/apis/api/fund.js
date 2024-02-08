2bb6b2c8b92d7f4e710081221de7350daf6692ef
최다희 <dahee121816@gmail.com>
2024-02-08 22:26:13 +0900

    Feat : ChildNav-FundModal

--- frontend/src/apis/api/fund.js
+++ frontend/src/apis/api/fund.js
@@ -4,18 +4,22 @@ const server = serverAxios();
 
 const url = '/fund';
 
+// 투자 항목 조회
 async function getFund(childId, success, fail) {
     await server.get(`${url}/confirm/${childId}`).then(success).catch(fail);
 }
 
+// 투자 내역 조회
 async function getFundHistory(childId, success, fail) {
     await server.get(`${url}/history/confirm/${childId}`).then(success).catch(fail);
 }
 
+// 투자 성공률 조회
 async function getRoi(childId, success, fail) {
     await server.get(`${url}/roi/${childId}`).then(success).catch(fail);
 }
 
+// 투자 상태 조회
 async function getStatus(childId, success, fail) {
     await server.get(`${url}/status/confirm/${childId}`).then(success).catch(fail);
 }
--- frontend/src/views/childFund/ChildFundManagement.jsx
+++ frontend/src/views/childFund/ChildFundManagement.jsx
@@ -17,25 +17,39 @@ export default function ChildFundManagement() {
     const [currentIndex, setCurrentIndex] = useState(0); // 모달 페이지
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [buyCoin, setBuyCoin] = useState('');
-    const [fund, setFund] = useState([]);
-    const [fundStatus, setFundStatus] = useState(0);
-    const [child, setChild] = useState([]);
-    const [fundHistory, setFundHistory] = useState([]);
-    const [labels, setLabels] = useState([]);
-    const [rates, setRates] = useState([]);
-    const [roi, setRoi] = useState([]);
+    const [fund, setFund] = useState([]); // 투자 항목 테이블
+    const [fundStatus, setFundStatus] = useState([]); // 투자 상태 테이블
+    const [child, setChild] = useState([]); // 자식 테이블(코인, 투자자산)
+    const [fundHistory, setFundHistory] = useState([]); // 투자 내역 테이블
+    const [labels, setLabels] = useState([]); // 그래프 라벨
+    const [rates, setRates] = useState([]); // 이익률
+    const [roi, setRoi] = useState([]); // 투자 성공률 테이블
     const [successRate, setSuccessRate] = useState(0);
-    const [avgFundMoney, setAvgFundMoney] = useState(0);
-    const [choice, setChoice] = useState('');
-    const [pnlRate, setPnlRate] = useState(0);
+    const [avgFundMoney, setAvgFundMoney] = useState(0); // 평균 투자 금액
+    const [choice, setChoice] = useState(''); // 아이의 베팅 선택
+    const [pnlRate, setPnlRate] = useState(0); // 이익률
     const [sellCoin, setSellCoin] = useState(''); // 판매할 도토리
     const [isBuyBtnActive, setIsBuyBtnActive] = useState(false);
     const [isSellBtnActive, setIsSellBtnActive] = useState(false);
     const [currentHaveCoin, setCurrentHaveCoin] = useState(30000); // 현재 보유금
     const [currentFundCoin, setCurrentFundCoin] = useState(10000); // 현재 투자금
     const [myChoice, setMyChoice] = useState(0); // 오늘 나의 선택
+    const [showDiv, setShowDiv] = useState(false); // 거래할 수 있는 시간이면 true
+
+    // 투자를 거래할 수 있는 시간인지 확인
+    useEffect(() => {
+        const currentTime = new Date();
+        const currentHour = currentTime.getHours();
+
+        // 현재 시간이 9시에서 17시 사이인지 확인
+        const isBetween9to5 = currentHour >= 8 && currentHour <= 19;
+
+        // 현재 시간이 9시에서 17시 사이이면 showDiv 상태를 true로 설정
+        setShowDiv(isBetween9to5);
+    }, []);
 
     useEffect(() => {
+        // 투자항목 테이블 조회
         getFund(
             childId,
             (success) => {
@@ -54,6 +68,7 @@ export default function ChildFundManagement() {
     }, []);
 
     useEffect(() => {
+        // 자식 테이블 조회
         getChild(
             childId,
             (success) => {
@@ -69,6 +84,8 @@ export default function ChildFundManagement() {
     }, []);
 
     useEffect(() => {
+        // 투자 내역 테이블 조회
+        // 최근 일주일 투자 현황의 라벨과 데이터를 뽑아냄
         getFundHistory(
             childId,
             (success) => {
@@ -82,7 +99,7 @@ export default function ChildFundManagement() {
                     let dataLog = new Date(row.dataLog);
                     let fDataLog = format(dataLog, 'yyyy.MM.dd');
                     if (differenceInDays(new Date(dateTime), new Date(fDataLog)) <= 7) {
-                        labelList.push(''); // 수정한곳임!!!!!!!!!!!
+                        labelList.push(fDataLog);
                         rateList.push((row.pnl < row.seedMoney ? -1 : 1) * row.yield);
                     }
                 });
@@ -99,6 +116,7 @@ export default function ChildFundManagement() {
     }, []);
 
     useEffect(() => {
+        // 투자 성공률 조회
         getRoi(
             childId,
             (success) => {
@@ -114,6 +132,7 @@ export default function ChildFundManagement() {
     }, []);
 
     useEffect(() => {
+        // 투자 상태 조회
         getStatus(
             childId,
             (success) => {
@@ -137,7 +156,7 @@ export default function ChildFundManagement() {
     const handleInputBuyChange = (e) => {
         const value = e.target.value;
         setBuyCoin(value);
-        setIsBuyBtnActive(value > 0 && value <= currentHaveCoin);
+        setIsBuyBtnActive(value > 0 && value <= child.fundMoney);
     };
 
     const handleInputSellChange = (e) => {
@@ -193,6 +212,7 @@ export default function ChildFundManagement() {
         },
     };
 
+    // 최근 일주일 투자현황 data
     const data = {
         labels,
         datasets: [
@@ -206,6 +226,9 @@ export default function ChildFundManagement() {
         ],
     };
 
+    // 아이의 투자 상태를 setChoice에 저장
+    // fundStatus 객체가 변경될 때마다 useEffect 콜백 함수를 실행
+    // setChoice 함수를 호출하여 choice 상태를 업데이트
     useEffect(() => {
         if (fundStatus && fundStatus.amount != 0) {
             setChoice(fundStatus.submit ? '성공' : '실패');
@@ -214,13 +237,17 @@ export default function ChildFundManagement() {
         }
     }, [fundStatus]);
 
+    // 투자 성공률 변동을 setSuccessRate에 저장
+    // 투자 성공률 roi 객체가 변경될 때마다 콜백 함수 실행
     useEffect(() => {
-        let isZero = roi.count;
+        let isZero = roi.count; // 투자 횟수
         let count = isZero == 0 ? 1 : isZero;
         let rate = (roi.success / count) * 100;
         setSuccessRate(rate);
     }, [roi]);
 
+    // 평균 투자금액, 이익률을 저장
+    // fundHistory 객체가 변경될 때마다 콜백 함수 실행
     useEffect(() => {
         let count = 0;
         let sumSeedMoney = 0;
@@ -234,11 +261,11 @@ export default function ChildFundManagement() {
         count = count == 0 ? 1 : count;
         sumSeedMoney = sumSeedMoney == 0 ? 1 : sumSeedMoney;
 
-        let avg = sumSeedMoney / count;
-        let pRate = (sumPnl / sumSeedMoney) * 100;
+        let avg = sumSeedMoney / count; // 평균 투자금액
+        let pRate = (sumPnl / sumSeedMoney) * 100; // 이익률
 
-        setPnlRate(pRate);
         setAvgFundMoney(avg);
+        setPnlRate(pRate);
     }, [fundHistory]);
 
     return (
@@ -253,7 +280,7 @@ export default function ChildFundManagement() {
                         <div className={styles.card1}>
                             <div className={styles.title1}>
                                 투자 항목
-                                {isFundItem ? (
+                                {isFundItem && showDiv ? (
                                     <div className={styles.startFund} onClick={openModal}>
                                         거래하러 가기
                                     </div>

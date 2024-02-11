c4e110a6f7b7c6ead158cb7fe606e20d2171e193
고은석 <ko2sist@gmail.com>
2024-02-11 23:36:43 +0900

    Fix: fix kafka url

--- backend-notification/src/main/java/com/ssafy/kdkd/notification/config/KafkaProducerConfig.java
+++ backend-notification/src/main/java/com/ssafy/kdkd/notification/config/KafkaProducerConfig.java
@@ -18,7 +18,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, NotificationMessage> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka.silvstone.xyz");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "i10b305.p.ssafy.io:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);

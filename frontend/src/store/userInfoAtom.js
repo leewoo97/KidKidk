e3702a6fb43c408902ce34628bcf972b2f74d4c7
chanmyeongwoo <chanmyeongwoo@naver.com>
2024-02-12 22:02:12 +0900

    Add: add recoilPersist

--- frontend/src/store/userInfoAtom.js
+++ frontend/src/store/userInfoAtom.js
@@ -1,6 +1,10 @@
+import { recoilPersist } from 'recoil-persist';
 import { atom } from 'recoil';
+
+const { persistAtom } = recoilPersist();
 
 export const userInfoState = atom({
     key: 'userInfo',
     default: null,
+    effects_UNSTABLE: [persistAtom],
 });

e98eb16b886fa98eb7610fb0fc79877f01418cc6
고은석 <ko2sist@gmail.com>
2024-02-12 16:36:04 +0900

    Fix: fix response setting

--- backend-notification/src/main/java/com/ssafy/kdkd/notification/controller/NotificationController.java
+++ backend-notification/src/main/java/com/ssafy/kdkd/notification/controller/NotificationController.java
@@ -21,9 +21,9 @@ public class NotificationController {
     public static final Long DEFAULT_TIMEOUT = 3600L * 1000;
 
     @GetMapping(value = "/subscribe/{userId}", produces = "text/event-stream")
-    public SseEmitter subscribe(@PathVariable String userId, @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) throws IOException {
+    public SseEmitter subscribe(@PathVariable String userId, @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId, HttpServletResponse response) throws IOException {
         System.out.println(lastEventId);
-        return notificationService.subscribe(userId, lastEventId);
+        return notificationService.subscribe(userId, lastEventId, response);
     }
 
     @PostMapping(value = "/publish")

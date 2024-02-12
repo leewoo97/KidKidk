e00a6a39ef84c31e06fe50d8e6638d69d3185b9f
chanmyeongwoo <chanmyeongwoo@naver.com>
2024-02-12 22:05:30 +0900

    Feat: feat parentNav

--- frontend/src/components/ParentNav.jsx
+++ frontend/src/components/ParentNav.jsx
@@ -1,11 +1,14 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 
 import ProfileNav from './ProfileNav';
 import Modal from 'react-modal';
-import { useState } from 'react';
+import { createRef, useEffect, useState, useRef } from 'react';
 import ParentAlarm from './ParentAlarm';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
-import { getChildList } from '@store/profile.js';
+import { getChildIds, childIdAtom } from '@store/childIdsAtom.js';
+import { userInfoState } from '@store/userInfoAtom.js';
+import { getChildList } from '@api/profile.js';
 
 import styles from './ParentNav.module.css';
 import s from 'classnames'; /* 클래스네임을 여러개 쓰기 위함 */
@@ -14,8 +17,20 @@ import kidImg from '@images/kidImg.jpg';
 
 function ParentNav() {
     const navigate = useNavigate();
+    const location = useLocation(); // 현재 url을 확인
     const [top, setTop] = useState(0);
     const a = ['5.5%', '18%', '29.5%'];
+
+    useEffect(() => {
+        // 페이지가 로드될 때 현재 URL을 확인하여 알맞은 탭을 활성화
+        const pathMap = {
+            '/parent/main': 0,
+            '/parent/job': 1,
+            '/parent/fundsaving': 2,
+        };
+        const currentTop = pathMap[location.pathname] || 0;
+        setTop(currentTop);
+    }, [location]);
 
     const handleMain = (num) => {
         setTop(num);
@@ -30,14 +45,32 @@ function ParentNav() {
         navigate(newPath);
     };
 
-    // 가상의 사용자 데이터...
-    const userData = [
-        {
-            user: '짱아',
-            job: '펫시터',
-        },
-        { user: '짱구', job: '환경 미화원' },
-    ];
+    // 탭 전환을 위한 childId 받아오기
+    const userInfo = useRecoilValue(userInfoState);
+    const userId = userInfo.userId;
+    //console.log(userId);
+    const [childIdsData, setChildIdsData] = useRecoilState(getChildIds);
+    useEffect(() => {
+        getChildList(
+            userId,
+            (success) => {
+                //console.log(success.data);
+                setChildIdsData(success.data);
+            },
+            (fail) => {
+                console.log(fail);
+            }
+        );
+    }, []);
+    //console.log(childIdsData);
+
+    const [parentChildId, setParentChildId] = useRecoilState(childIdAtom);
+    const childIdRefs = useRef([]);
+    const handleTabClick = (childId) => {
+        console.log('Clicked childId:', childId);
+        setParentChildId(childId);
+        window.location.reload();
+    };
 
     // 부모 모달 창
     const [parentAlarmOpen, setparentAlarmOpen] = useState(false);
@@ -78,12 +111,24 @@ function ParentNav() {
                 <ProfileNav />
                 {/* 아이 탭 */}
                 <div className={styles.parentChildTab}>
-                    {userData.map((data) => (
-                        <div key={data.user} className={styles.parentChildTabContainer}>
-                            <div>{data.user}</div>&nbsp;&nbsp;
-                            <div>{data.job}</div>
-                        </div>
-                    ))}
+                    {childIdsData.map((data, index) => {
+                        const childIdRef = createRef();
+
+                        // 배열에 각 div에 대한 ref 추가
+                        childIdRefs.current[index] = childIdRef;
+
+                        return (
+                            <div
+                                key={data.nickname}
+                                className={styles.parentChildTabContainer}
+                                name={data.childId}
+                                onClick={() => handleTabClick(data.childId)}
+                                ref={childIdRef}
+                            >
+                                <div>{data.nickname}</div>
+                            </div>
+                        );
+                    })}
                 </div>
                 <div onClick={() => setparentAlarmOpen(true)}>
                     <img src={bell} className={styles.alarm} />
